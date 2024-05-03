c95eb7ac903aee4b6cc40915aa3087614aecfe7a
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-05-04 00:40:40 +0530

    all configs

---
+++ nvim/init.lua
@@ -0,0 +1,45 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("config.lazy")
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
+local o = vim.o
+o.cursorline = false
+o.expandtab = true
+o.smartindent = true
+o.tabstop = 4
+o.shiftwidth = 4
+vim.opt.list = false
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 0
+vim.opt.laststatus = 0
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.wrap = false
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" })
+vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitkeep = "cursor"
+vim.opt.mouse = ""
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append({ "r" })
